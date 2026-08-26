89e919518469e661faa6039cde30c8867a18c85c
danfulton72 <42965092+danfulton72@users.noreply.github.com>
2026-08-26 10:22:57 +0100

    Add Facebox constants

---
+++ custom_components/facebox/const.py
@@ -0,0 +1,13 @@
+"""Constants for the Facebox integration."""
+
+DOMAIN = "facebox"
+CLASSIFIER = "facebox"
+DATA_FACEBOX = "facebox_classifiers"
+SERVICE_TEACH_FACE = "teach_face"
+LEGACY_SERVICE_TEACH_FACE = "facebox_teach_face"
+ATTR_BOUNDING_BOX = "bounding_box"
+ATTR_IMAGE_ID = "image_id"
+ATTR_MATCHED = "matched"
+FILE_PATH = "file_path"
+DEFAULT_PORT = 8080
+REQUEST_TIMEOUT = 10
